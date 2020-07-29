357e7ee05330d092be6a5e413e85bb7d55852b3c
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-07-29 15:31:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const expressValidator = require('express-validator')
+require('dotenv').config()
+
+//import
+const userRoutes = require('./routes/user')
+
+//app
+const app = express()
+
+//db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', (err) => {
+  console.log(`DB connection error: ${err.message}`)
+})
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
+
+//routes middleware
+app.use('/api', userRoutes)
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})

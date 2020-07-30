dfd9e93faffbc14a5955caf7897572af034bfaf3
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-07-30 23:47:45 +0200

    update product

--- controllers/product.js
+++ controllers/product.js
@@ -77,3 +77,73 @@ exports.create = (req, res) => {
     })
   })
 }
+
+// remove product
+exports.remove = (req, res) => {
+  let product = req.product
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      })
+    }
+    res.json({
+      message: 'Product deleted successfully',
+    })
+  })
+}
+
+// update product
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm()
+  form.keepExtensions = true
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Image could not be uploaded',
+      })
+    }
+
+    // check for all fields
+    const { name, description, price, category, quantity, shipping } = fields
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: 'All fields are required.',
+      })
+    }
+
+    let product = req.product
+    product = _.extend(product, fields)
+
+    // 1kb = 1000
+    // 1mb = 1000000
+
+    if (files.photo) {
+      // console.log('FILES PHOTO: ', files.photo)
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1Mb size.',
+        })
+      }
+      product.photo.data = fs.readFileSync(files.photo.path)
+      product.photo.contentType = files.photo.type
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        })
+      }
+      res.json(result)
+    })
+  })
+}

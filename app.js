48cf82236ebcba80ce0f54668655b96aaabbb178
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-07-30 12:01:10 +0200

    category model, route and controller

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').config()
 //import
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
 
 //app
 const app = express()
@@ -35,6 +36,7 @@ app.use(expressValidator())
 //routes middleware
 app.use('/api', authRoutes)
 app.use('/api', userRoutes)
+app.use('/api', categoryRoutes)
 
 const port = process.env.PORT || 8000
 

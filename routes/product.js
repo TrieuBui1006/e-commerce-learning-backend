ca9c76a4ebae587acc3de5dcaaccfb0025ade2d5
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-07-31 22:49:52 +0200

    send product photo

--- routes/product.js
+++ routes/product.js
@@ -9,6 +9,11 @@ const {
   read,
   remove,
   update,
+  list,
+  listRelated,
+  listCategories,
+  listBySearch,
+  photo,
 } = require('../controllers/product')
 
 router.get('/product/:productId', read)
@@ -27,6 +32,11 @@ router.put(
   isAdmin,
   update
 )
+router.get('/products', list)
+router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
+router.post('/products/by/search', listBySearch)
+router.get('/product/photo/:productId', photo)
 
 router.param('userId', userById)
 router.param('productId', productById)

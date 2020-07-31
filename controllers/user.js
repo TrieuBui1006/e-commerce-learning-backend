1a3f5cfc76df880cfb2fb61fe28a8842fe025334
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-07-31 23:07:51 +0200

    user read and update

--- controllers/user.js
+++ controllers/user.js
@@ -16,3 +16,29 @@ exports.userById = (req, res, next, id) => {
     next()
   })
 }
+
+// read user by Id
+exports.read = (req, res) => {
+  req.profile.hashed_password = undefined
+  req.profile.salt = undefined
+  return res.json(req.profile)
+}
+
+// update user
+exports.update = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: 'you are not authorized to perform this action',
+        })
+      }
+      user.profile.hashed_password = undefined
+      user.profile.salt = undefined
+      res.json(user)
+    }
+  )
+}

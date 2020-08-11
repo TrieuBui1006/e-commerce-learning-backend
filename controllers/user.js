bb03dee840601a986f0d0218f47f628670aad3c0
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-08-12 00:10:03 +0200

    refactor update user

--- controllers/user.js
+++ controllers/user.js
@@ -25,22 +25,64 @@ exports.read = (req, res) => {
 }
 
 // update user
+// exports.update = (req, res) => {
+//   User.findByIdAndUpdate(
+//     { _id: req.profile._id },
+//     { $set: req.body },
+//     { new: true },
+//     (err, user) => {
+//       if (err) {
+//         return res.status(400).json({
+//           error: 'you are not authorized to perform this action',
+//         })
+//       }
+//       user.profile.hashed_password = undefined
+//       user.profile.salt = undefined
+//       res.json(user)
+//     }
+//   )
+// }
+
 exports.update = (req, res) => {
-  User.findByIdAndUpdate(
-    { _id: req.profile._id },
-    { $set: req.body },
-    { new: true },
-    (err, user) => {
-      if (err) {
+  // console.log('UPDATE USER - req.user', req.user, 'UPDATE DATA', req.body);
+  const { name, password } = req.body
+
+  User.findOne({ _id: req.profile._id }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: 'User not found',
+      })
+    }
+    if (!name) {
+      return res.status(400).json({
+        error: 'Name is required',
+      })
+    } else {
+      user.name = name
+    }
+
+    if (password) {
+      if (password.length < 6) {
         return res.status(400).json({
-          error: 'you are not authorized to perform this action',
+          error: 'Password should be min 6 characters long',
+        })
+      } else {
+        user.password = password
+      }
+    }
+
+    user.save((err, updatedUser) => {
+      if (err) {
+        console.log('USER UPDATE ERROR', err)
+        return res.status(400).json({
+          error: 'User update failed',
         })
       }
-      user.profile.hashed_password = undefined
-      user.profile.salt = undefined
-      res.json(user)
-    }
-  )
+      updatedUser.hashed_password = undefined
+      updatedUser.salt = undefined
+      res.json(updatedUser)
+    })
+  })
 }
 
 exports.addOrderToUserHistory = (req, res, next) => {
